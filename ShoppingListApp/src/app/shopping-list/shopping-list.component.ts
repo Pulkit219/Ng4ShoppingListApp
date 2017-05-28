3b5c8e245e3ee3e5ece84ea291de63288acba95d
Pulkit219 <ascension1110@gmail.com>
2017-05-27 22:22:58 -0400

    17th comm
    adding shopping list edit section

--- ShoppingListApp/src/app/shopping-list/shopping-list.component.ts
+++ ShoppingListApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-ingredients =[];
+ingredients:Ingredient[] =[
+new Ingredient('yogurt', 3),
+new Ingredient('tomoato',5),
+
+];
   constructor() { }
 
   ngOnInit() {
